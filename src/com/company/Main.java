69e8b18a960f04b5ea37a3d5239bb8b6543755c5
LucasMostafa <mostafalucas@gmail.com>
2021-03-22 23:46:56 -0300

    Ejercicio 1 finalizado

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import com.company.ejercicio1.rectangle;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //Ejercicio 1
+        rectangle rectangle= new rectangle();
+        System.out.println("Ancho: " + rectangle.getBroad());
+        System.out.println("Alto: " + rectangle.getHigh());
+        System.out.println("Area: " + rectangle.calculateArea());
+        System.out.println("Perimetro: " + rectangle.calculatePerimeter());
+
+        rectangle.setBroad(100);
+        rectangle.setHigh(50);
+        System.out.println("Area: " + rectangle.calculateArea());
+        System.out.println("Perimetro: " + rectangle.calculatePerimeter());
     }
 }
--- src/com/company/ejercicio1/rectangle.java
+++ src/com/company/ejercicio1/rectangle.java
@@ -1,4 +1,39 @@
 package com.company.ejercicio1;
 
 public class rectangle {
+
+    private double broad = 1.0;
+    private double high = 1.0;
+
+    public rectangle() {
+    }
+
+    public rectangle(double broad, double high) {
+        this.broad = broad;
+        this.high = high;
+    }
+
+    public double getBroad() {
+        return broad;
+    }
+
+    public void setBroad(double broad) {
+        this.broad = broad;
+    }
+
+    public double getHigh() {
+        return high;
+    }
+
+    public void setHigh(double high) {
+        this.high = high;
+    }
+
+    public double calculateArea() {
+        return broad * high;
+    }
+
+    public double calculatePerimeter() {
+        return 2 * (broad + high);
+    }
 }
